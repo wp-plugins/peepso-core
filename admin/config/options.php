720fca108d5ee4b08748e7dd9ed45a5573366551
peepso <peepso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-12 07:47:59 +0000

    initial trunk commit
    git-svn-id: https://plugins.svn.wordpress.org/peepso-core/trunk@1197372 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/config/options.php
@@ -0,0 +1,52 @@
+<?php
+	$peepso_admin = PeepSoAdmin::get_instance();
+	$peepso_admin->admin_notices();
+?>
+<div  id="peepso" class="wrap">
+	<?php
+	echo $config->form_open();
+	?>
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="row">
+					<!-- Left column -->
+					<div class="col-xs-12 col-sm-6">
+						<?php do_meta_boxes('peepso_page_peepso-config', 'left', null); ?>
+					</div>
+					<!-- Right column -->
+					<div class="col-xs-12 col-sm-6">
+						<?php do_meta_boxes('peepso_page_peepso-config', 'right', null); ?>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="row">
+					<div class="col-xs-12">
+					<?php do_meta_boxes('peepso_page_peepso-config', 'full', null); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="form-actions center">
+					<button class="btn btn-info" type="submit">
+						<i class="ace-icon fa fa-check bigger-110"></i>
+						<?php _e('Save Settings', 'peepso'); ?>
+					</button>
+
+					&nbsp; &nbsp; &nbsp;
+					<button class="btn" type="reset">
+						<i class="ace-icon fa fa-undo bigger-110"></i>
+						<?php _e('Reset', 'peepso'); ?>
+					</button>
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
